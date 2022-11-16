11406478108bb8580434e7ee155d0e7ca27e7eb5
p4535992 <tentimarco0@gmail.com>
2022-11-16 14:56:08 +0100

    first commit

--- README.md
+++ README.md
@@ -1,17 +1,67 @@
-# Lazy Money module for Foundry VTT
-## NO LONGER MAINTAINED
+# Final Blow
+
+![Latest Release Download Count](https://img.shields.io/github/downloads/p4535992/foundryvtt-lazymoney-dnd5e/latest/module.zip?color=2b82fc&label=DOWNLOADS&style=for-the-badge)
+
+[![Forge Installs](https://img.shields.io/badge/dynamic/json?label=Forge%20Installs&query=package.installs&suffix=%25&url=https%3A%2F%2Fforge-vtt.com%2Fapi%2Fbazaar%2Fpackage%2Ffinal-blow&colorB=006400&style=for-the-badge)](https://forge-vtt.com/bazaar#package=final-blow)
+
+![Foundry Core Compatible Version](https://img.shields.io/badge/dynamic/json.svg?url=https%3A%2F%2Fraw.githubusercontent.com%2Fp4535992%2Ffoundryvtt-lazymoney-dnd5e%2Fmaster%2Fsrc%2Fmodule.json&label=Foundry%20Version&query=$.compatibility.verified&colorB=orange&style=for-the-badge)
+
+![Latest Version](https://img.shields.io/badge/dynamic/json.svg?url=https%3A%2F%2Fraw.githubusercontent.com%2Fp4535992%2Ffoundryvtt-lazymoney-dnd5e%2Fmaster%2Fsrc%2Fmodule.json&label=Latest%20Release&prefix=v&query=$.version&colorB=red&style=for-the-badge)
+
+[![Foundry Hub Endorsements](https://img.shields.io/endpoint?logoColor=white&url=https%3A%2F%2Fwww.foundryvtt-hub.com%2Fwp-json%2Fhubapi%2Fv1%2Fpackage%2Ffinal-blow%2Fshield%2Fendorsements&style=for-the-badge)](https://www.foundryvtt-hub.com/package/final-blow/)
+
+![GitHub all releases](https://img.shields.io/github/downloads/p4535992/foundryvtt-lazymoney-dnd5e/total?style=for-the-badge)
+
+[![Translation status](https://weblate.foundryvtt-hub.com/widgets/final-blow/-/287x66-black.png)](https://weblate.foundryvtt-hub.com/engage/final-blow/)
+
+### If you want to buy me a coffee [![alt-text](https://img.shields.io/badge/-Patreon-%23ff424d?style=for-the-badge)](https://www.patreon.com/p4535992)
+
 Easily add or remove currency with automatic conversion and no overdraft.
-## Preview
-![](https://i.imgur.com/IUml0iX.gif)
+
+![img](wiki/IUml0iX.gif)
+
+## NOTE: If you are a javascript developer and not a typescript developer, you can just use the javascript files under the dist folder
+
+
 ## Installation
-1. Open Foundry's Setup screen
-2. Switch to the "Add-On Modules" tab
-3. Click "Install Module"
-4. Paste `https://github.com/DeVelox/lazymoney/raw/master/module.json` into the "Manifest URL" field
-5. Click "Install"
-## Notes
-- When removing currency the module will remove it from higher denominations when necessary.
-- When adding currency the module will simply add it without implicit conversion (by default).
-- The module won't allow removing more currency than is available in total.
-## Compatibility
-Currently supports any dnd5e character sheet that doesn't change the name of currency input fields.
+
+It's always easiest to install modules from the in game add-on browser.
+
+To install this module manually:
+1.  Inside the Foundry "Configuration and Setup" screen, click "Add-on Modules"
+2.  Click "Install Module"
+3.  In the "Manifest URL" field, paste the following url:
+`https://raw.githubusercontent.com/p4535992/foundryvtt-lazymoney-dnd5e/master/src/module.json`
+4.  Click 'Install' and wait for installation to complete
+5.  Don't forget to enable the module in game using the "Manage Module" button
+
+### socketLib
+
+This module uses the [socketLib](https://github.com/manuelVo/foundryvtt-socketlib) library like a dependency. It is a hard dependency and it is recommended for the best experience and compatibility with other modules.
+
+### active-effect-manager-lib
+
+This module uses the [active-effect-manager-lib](https://github.com/p4535992/foundryvtt-active-effect-manager-lib) library like a dependency. It is a hard dependency and it is recommended for the best experience and compatibility with other modules.
+
+## Known Issue\Limitation
+
+## Api
+
+
+## [Changelog](./CHANGELOG.md)
+
+## Issues
+
+Any issues, bugs, or feature requests are always welcome to be reported directly to the [Issue Tracker](https://github.com/p4535992/foundryvtt-lazymoney-dnd5e/issues ), or using the [Bug Reporter Module](https://foundryvtt.com/packages/bug-reporter/).
+
+## License
+
+This package is under an [MIT license](LICENSE) and the [Foundry Virtual Tabletop Limited License Agreement for module development](https://foundryvtt.com/article/license/).
+
+## Acknowledgements
+
+Bootstrapped with League of Extraordinary FoundryVTT Developers  [foundry-vtt-types](https://github.com/League-of-Foundry-Developers/foundry-vtt-types).
+
+## Credit
+
+Thanks to anyone who helps me with this code! I appreciate the user community's feedback on this project!
---
+++ src/scripts/lazymoney.js
@@ -0,0 +1,274 @@
+Hooks.once("ready", () => {
+  console.log("lazymoney | Initializing lazymoney");
+   Object.keys(CONFIG.Actor.sheetClasses.character).forEach(key => {
+		doHook(key);
+  });
+  Object.keys(CONFIG.Actor.sheetClasses.npc).forEach(key => {
+		doHook(key);
+  });
+});
+
+Hooks.once("init", () => {
+  game.settings.register("lazymoney", "addConvert", {
+    name: "Convert when adding money",
+    hint: "Automatically convert currency to higher denominations when adding money instead of the default behaviour of preserving the denominations you receive.",
+    scope: "client",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+});
+
+Hooks.once("init", () => {
+  game.settings.register("lazymoney", "ignoreElectrum", {
+    name: "Ignore electrum",
+    hint: "When converting, ignore electrum. This won't affect directly adding or removing it, except in the case when \"Convert when adding money\" is also enabled.",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+});
+
+Hooks.once("init", () => {
+  game.settings.register("lazymoney", "chatLog", {
+    name: "Chat log",
+    hint: "Whisper any currency changes to the GM.",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+});
+
+const signCase = {
+  add: '+',
+  subtract: '-',
+  equals: '=',
+  default: ' '
+};
+
+function _onChangeCurrency(ev) {
+  const input = ev.target;
+  const actor = ev.data.app.actor;
+  const sheet = ev.data.app.options;
+  const money = ev.data.app.actor.data.data.currency;
+  const denom = input.name.split(".")[2];
+  const value = input.value;
+  let sign = signCase.default;
+  Object.values(signCase).forEach(val => {
+    if (value.includes(val)) {
+      sign = val;
+    }
+  });
+  const splitVal = value.split(sign);
+  let delta;
+  if (splitVal.length > 1) {
+    delta = Number(splitVal[1]);
+  }
+  else {
+    delta = Number(splitVal[0]);
+    chatLog(actor, `Replaced ${money[denom]} ${denom} with ${delta} ${denom}.`);
+    return;
+  }
+
+  let newAmount = {};
+  if (!(denom === "ep" && game.settings.get("lazymoney", "ignoreElectrum"))) {
+    switch (sign) {
+      case signCase.add:
+        newAmount = addMoney(money, delta, denom);
+        chatLog(actor, `Added ${delta} ${denom}.`);
+        break;
+      case signCase.subtract:
+        newAmount = removeMoney(money, delta, denom);
+        chatLog(actor, `Removed ${delta} ${denom}.`);
+        if (!newAmount) {
+          flash(input);
+          newAmount = money;
+        }
+        break;
+      case signCase.equals:
+        newAmount = updateMoney(money, delta, denom);
+        chatLog(actor, `Replaced ${money[denom]} ${denom} with ${delta} ${denom}.`);
+        break;
+      default:
+        newAmount = updateMoney(money, delta, denom);
+        chatLog(actor, `Replaced ${money[denom]} ${denom} with ${delta} ${denom}.`);
+        break;
+    }
+  }
+  if (Object.keys(newAmount).length > 0) {
+    sheet.submitOnChange = false;
+    actor.update({ "data.currency": newAmount }).then(() => {
+      input.value = getProperty(actor.data, input.name);
+      sheet.submitOnChange = true;
+    }).catch(console.log.bind(console));
+  }
+}
+
+function chatLog(actor, money) {
+  if (game.settings.get("lazymoney", "chatLog")) {
+    const msgData = { content: money, speaker: ChatMessage.getSpeaker({ actor: actor }), whisper: ChatMessage.getWhisperRecipients("GM") };
+    return ChatMessage.create(msgData);
+  }
+}
+
+function getCpValue() {
+  let cpValue = {
+    pp: { value: 1000, up: "", down: "gp" },
+    gp: { value: 100, up: "pp", down: "ep" },
+    ep: { value: 50, up: "gp", down: "sp" },
+    sp: { value: 10, up: "ep", down: "cp" },
+    cp: { value: 1, up: "sp", down: "" }
+  };
+  let total = 1;
+  if (parseFloat(game.data.system.data.version) >= 1.5) {
+    const convert = CONFIG.DND5E.currencies;
+    Object.values(convert).reverse().forEach(v => {
+      if (v.conversion !== undefined) {
+        total *= v.conversion.each;
+        cpValue[v.conversion.into].value = total;
+      }
+    });
+  }
+  else {
+    const convert = CONFIG.DND5E.currencyConversion;
+    Object.values(convert).forEach(v => {
+      total *= v.each;
+      cpValue[v.into].value = total;
+    });
+  }
+
+  if (game.settings.get("lazymoney", "ignoreElectrum")) {
+    cpValue.gp.down = "sp";
+    cpValue.sp.up = "gp";
+    delete cpValue.ep;
+  }
+  return cpValue;
+}
+
+function getDelta(delta, denom) {
+  const cpValue = getCpValue();
+  let newDelta = {};
+  delta *= cpValue[denom].value;
+  for (let key in cpValue) {
+    let intDiv = ~~(delta / cpValue[key].value);
+    if (intDiv > 0) {
+      newDelta[key] = intDiv;
+      delta %= cpValue[key].value;
+    }
+  }
+  return newDelta;
+}
+
+function scaleDown(oldAmount, denom) {
+  const cpValue = getCpValue();
+  const up = cpValue[denom].up;
+  let newAmount = oldAmount;
+  if (newAmount[up] > 0) {
+    newAmount[up] -= 1;
+    newAmount[denom] += ~~(cpValue[up].value / cpValue[denom].value);
+    return newAmount;
+  }
+  else if (newAmount[up] === 0) {
+    newAmount = scaleDown(newAmount, up);
+    scaleDown(newAmount, denom);
+    return newAmount;
+  }
+  else {
+    return false;
+  }
+}
+
+function addMoney(oldAmount, delta, denom) {
+  const cpValue = getCpValue();
+  let newAmount = {};
+  if (game.settings.get("lazymoney", "addConvert")) {
+    let cpDelta = delta * cpValue[denom].value;
+    for (let key in cpValue) {
+      newAmount[key] = oldAmount[key] + ~~(cpDelta / cpValue[key].value);
+      cpDelta %= cpValue[key].value;
+    }
+  }
+  else {
+    newAmount[denom] = oldAmount[denom] + delta;
+  }
+  return newAmount;
+}
+
+function removeMoney(oldAmount, delta, denom) {
+  const cpValue = getCpValue();
+  let newAmount = oldAmount;
+  let newDelta = {};
+  let down;
+  if (oldAmount[denom] >= delta) {
+    newAmount[denom] = oldAmount[denom] - delta;
+    return newAmount;
+  }
+  else {
+    newDelta = getDelta(delta, denom);
+    delta = delta * cpValue[denom].value;
+  }
+  if (totalMoney(oldAmount) >= delta) {
+    for (let [key, value] of Object.entries(newDelta)) {
+      if (newAmount[key] >= value) {
+        newAmount[key] -= value;
+      }
+      else if (scaleDown(newAmount, key)) {
+        newAmount[key] -= value;
+      }
+      else {
+        newAmount = oldAmount;
+        while (newAmount[key] <= value && totalMoney(newAmount) > 0 && key !== "cp") {
+          down = cpValue[key].down;
+          value -= newAmount[key];
+          newAmount[key] = 0;
+          value *= ~~(cpValue[key].value / cpValue[down].value);
+          key = down;
+        }
+        newAmount[key] -= value;
+      }
+    }
+    return newAmount;
+  }
+  else {
+    return false;
+  }
+}
+
+function updateMoney(oldAmount, delta, denom) {
+  let newAmount = {};
+  newAmount[denom] = delta;
+  return newAmount;
+}
+
+function totalMoney(money) {
+  const cpValue = getCpValue();
+  let total = 0;
+  for (let key in cpValue) {
+    total += money[key] * cpValue[key].value;
+  }
+  return total;
+}
+
+function flash(input) {
+  input.style.backgroundColor = "rgba(255,0,0,0.5)";
+  setTimeout(() => {
+    input.style.backgroundColor = "";
+  }, 150);
+}
+
+function doHook(key)
+{
+	let sheet = key.split(".")[1];
+	try{
+    Hooks.on("render" + sheet, (app, html, data) => {
+      html.find("input[name^='data.currency']").off("change");
+      html
+        .find("input[name^='data.currency']")
+        .change({ app: app, data: data }, _onChangeCurrency);
+    });
+	}catch(error){
+		console.warn("lazymoney can't hook to "+key);
+	}
+}
